460522f17d05fc291f382d548f01cda1c521946c
Klaudiusz Zembrzuski <moa21@o2.pl>
2022-02-04 19:45:54 +0100

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿namespace PropMZ.Controllers
+{
+    using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Models;
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger) => _logger = logger;
+
+        public IActionResult Index() => View();
+
+        public IActionResult AboutUs() => View();
+
+        public IActionResult Ethos() => View();
+
+        public IActionResult OurVision() => View();
+
+        public IActionResult Learn() => View();
+
+        public IActionResult Privacy() => View();
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() => View(new ErrorViewModel
+                                             { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PropMZ.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
